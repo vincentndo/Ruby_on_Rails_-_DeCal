0ef3ba5e6dd94cd89350c55b335165823b8c0127
Vincent Ninh DO <vincentnhdo@gmail.com>
2018-09-21 23:25:45 -0700

    Done hw1

--- homework/hw1/foobar.rb
+++ homework/hw1/foobar.rb
@@ -4,5 +4,6 @@ class Foobar
     # Class method, not an instance method
     # Call with `Foobar.baz`
     # Q4 CODE HERE
+    a.map { |e| e.to_i + 2 }.select { |e| e % 2 == 0 && e < 10 }.uniq.sum
   end
 end
--- homework/hw1/hw1.rb
+++ homework/hw1/hw1.rb
@@ -1,16 +1,16 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
-
+  (a + b) ** 2
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
-
+  a.sort.map{|e| e + 1}
 end
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
-
+  first_name + ' ' + last_name
 end
 
 def blockin_time(a)
@@ -49,4 +49,5 @@ def scrabble(word)
     z: 10,
   }
   # Q5 CODE HERE
+  word.split('').map{ |e| values[e.intern] }.sum
 end
